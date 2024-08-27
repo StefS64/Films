74640c626ecebb5b723a24569af2b71776a7fb8f
StefS64 <stefan.swierczewski@gmail.com>
2024-08-27 23:27:27 +0200

    new_categories_plus_created_empty_scripts

---
+++ all_films/add_new_film_or_change_catagories.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO:
+# add flags to clear all not selected categories except all_films and Ranking
+
+# otherwise create links only in the added categories
+
+# also add the ability to create new catagories
+
