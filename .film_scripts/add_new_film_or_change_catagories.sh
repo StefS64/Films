914857ab64d89a96c33cb613744a8b93da4733ad
StefS64 <stefan.swierczewski@gmail.com>
2024-09-07 13:38:06 +0200

    added description to list links and add films script

--- .film_scripts/add_new_film_or_change_catagories.sh
+++ .film_scripts/add_new_film_or_change_catagories.sh
@@ -6,4 +6,7 @@
 # otherwise create links only in the added categories
 
 # also add the ability to create new catagories
-
+# flags:	-n(new instance, overwrites all_instances already created)
+#			-u(update doesn't overwrite already given catagories) 
+#			-d delete a film
+#			ATTENTION the ranking category can only have one instance.
--- .film_scripts/list_all_links.sh
+++ .film_scripts/list_all_links.sh
@@ -2,3 +2,5 @@
 
 
 # TODO a script that given a file lists all more info in project files
+#	Flags: -s to string creates a string that informs about all of the this
+# add id-s to categories for ease of adding new ones. Easy way of decoding Catagories 01010101001010111010.
